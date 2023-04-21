5dac6d7704ba1dc003c3eb3507bb625ffef2cc3b
Luís Jesus <luisjesus__2003@outlook.com>
2023-04-21 22:29:11 +0100

    fixed details

--- pages/about.php
+++ pages/about.php
@@ -2,11 +2,12 @@
 declare(strict_types = 1);
 
 require_once(__DIR__ .  '/../templates/common.tpl.php');
-?>
+require_once(__DIR__ . '/../utils/session.php');
 
-<?php
+$session = new Session();
+
   drawHeader();
-  drawNavbar();
+  drawNavbar($session);
   ?>
     <main>
         <p>
--- pages/userTicket.php
+++ pages/userTicket.php
@@ -1,24 +1,24 @@
 <?php 
-declare(strict_types = 1);
+  declare(strict_types = 1);
 
-require_once(__DIR__ .  '/../templates/common.tpl.php');
-require_once(__DIR__ .  '/../templates/ticket.tpl.php');
+  require_once(__DIR__ .  '/../templates/common.tpl.php');
+  require_once(__DIR__ .  '/../templates/ticket.tpl.php');
+  require_once(__DIR__ . '/../utils/session.php');
 
+  $session = new Session();
+  drawHeader();
+  drawNavbar($session);
+  drawSearchbar();
 ?>
 
-  <?php
-  drawHeader();
-  drawNavbar();
-  drawSearchbar();
-  ?>
-  <main>
-  <?php 
+<main>
+<?php 
     drawTicket();
     drawTicket();
     drawTicket();
     drawTicket();
-  ?>
+?>
   </main>
-  <?php
+<?php
   drawFooter();
 ?>
