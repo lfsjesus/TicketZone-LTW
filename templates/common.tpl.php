a701eccbe14988f6a4801a3b8444913758db13a4
Luís Jesus <luisjesus__2003@outlook.com>
2023-04-24 11:59:24 +0100

    design is now responsive

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -7,6 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
     <script src="../javascript/script.js" defer></script>
   </head> 
   <body>           
