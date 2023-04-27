4d31eb1239da869c83eb99672e1c655eb3269b48
Luís Jesus <luisjesus__2003@outlook.com>
2023-04-27 18:41:55 +0100

    replaced table headers with dropdown menus

--- pages/userTicket.php
+++ pages/userTicket.php
@@ -27,12 +27,33 @@
       <thead>
         <tr>
           <th><input type="checkbox" id="select-all" name="select-all" value="select-all"></th>
-          <th>Author</th>
+          <th>
+            <select name="author" id="author">
+              <option value="all">All</option>
+          </th>
           <th>Message</th>
-          <th>Assignee</th>
-          <th>Status</th>
-          <th>Priority</th>
-          <th>Last Update</th>
+          <th>
+            <select value="Assignee">
+            <option value="all">All</option>
+
+          </th>
+          <th>
+            <select name="status" id="status">
+              <option value="all">All</option>
+              <option value="open">Open</option>
+              <option value="closed">Closed</option>
+          </th>
+          <th>
+            <select name="priority" id="priority">
+              <option value="high">High</option>
+              <option value="medium">Medium</option>
+              <option value="low">Low</option>
+          </th>
+          <th>
+            <select name="date" id="date">
+              <option value="newest">Newest</option>
+              <option value="oldest">Oldest</option>
+          </th>
         </tr>
       </thead>
       <tbody>
